ac64514d002b4f5e50b48aafda8cd82b62f9ec41
Jakexsc <649199985@qq.com>
2020-10-18 01:49:44 +0800

    堆 -> 删除操作

--- src/com/xsc/heapstudy/HeapSort.java
+++ src/com/xsc/heapstudy/HeapSort.java
@@ -28,7 +28,7 @@ public class HeapSort {
     }
 
     /**
-     * 插入操作
+     * 插入操作 -> 自下往上的堆化
      *
      * @param data 插入的值
      */
@@ -38,7 +38,7 @@ public class HeapSort {
             return;
         }
         // 插入值
-        arr[count++] = data;
+        arr[++count] = data;
         // 记录插入的下标
         int i = count;
         // 如果不是根节点而且插入的值比父节点大
@@ -47,6 +47,50 @@ public class HeapSort {
             swap(arr, i, i / 2);
             // 将节点下标变为父节点下标
             i = i / 2;
+        }
+    }
+
+    /**
+     * 删除操作
+     *
+     * @param data 删除的值
+     */
+    private void delete(int data) {
+        if (count == 0) {
+            return;
+        }
+        arr[1] = arr[count--];
+        heapify(arr, count, 1);
+    }
+
+    /**
+     * 堆化 -> 自上往下的堆化
+     *
+     * @param arr   堆
+     * @param count 最后一个值的小标
+     * @param i     堆顶
+     */
+    private void heapify(int[] arr, int count, int i) {
+        while (true) {
+            // 记录交换的下标 -> 初始化为堆顶
+            int maxPos = i;
+            // 如果左节点比父节点还大
+            if (i * 2 <= count && arr[i] < arr[i * 2]) {
+                // 记录左节点位置
+                maxPos = i * 2;
+            }
+            // 如果右节点比左节点或比父节点还大
+            if (i * 2 + 1 <= count && arr[maxPos] < arr[i * 2 + 1]) {
+                maxPos = i * 2 + 1;
+            }
+            // 如果堆顶是最大值，直接退出交换位置
+            if (maxPos == i) {
+                break;
+            }
+            // 交换父节点与最大值的位置
+            swap(arr, i, maxPos);
+            // 记录下一次循环的下标，就是左节点或右节点
+            i = maxPos;
         }
     }
 
