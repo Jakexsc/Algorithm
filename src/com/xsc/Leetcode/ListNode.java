e3f7943944a36523953be80f1bf1ca88b667b5c7
jakesc <649199985@qq.com>
2022-04-28 00:21:55 +0800

    commit re Algorithm

---
+++ src/com/xsc/Leetcode/ListNode.java
@@ -0,0 +1,30 @@
+package com.xsc.Leetcode;
+
+/**
+ * @author Jakexsc
+ * 2022/4/27
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    @Override
+    public String toString() {
+        return "ListNode{" +
+                "val=" + val +
+                ", next=" + next +
+                '}';
+    }
+}
---
+++ src/com/xsc/Leetcode/ReverseList206.java
@@ -0,0 +1,42 @@
+package com.xsc.Leetcode;
+
+/**
+ * 力扣206 - 反转链表
+ * 输入：head = [1,2,3,4,5]
+ * 输出：[5,4,3,2,1]
+ *
+ * @author Jakexsc
+ * 2022/4/27
+ */
+public class ReverseList206 {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode reverseList = null;
+        while (head != null) {
+            ListNode listNode = reverseList;
+            ListNode next = head.next;
+            reverseList = head;
+            reverseList.next = listNode;
+            head = next;
+        }
+        return reverseList;
+    }
+
+    public static void main(String[] args) {
+        ListNode listNode1 = new ListNode(1);
+        ListNode listNode2 = new ListNode(2);
+        ListNode listNode3 = new ListNode(3);
+        ListNode listNode4 = new ListNode(4);
+        ListNode listNode5 = new ListNode(5);
+        listNode1.next = listNode2;
+        listNode2.next = listNode3;
+        listNode3.next = listNode4;
+        listNode4.next = listNode5;
+
+        ReverseList206 reverseList206 = new ReverseList206();
+        ListNode reverseList = reverseList206.reverseList(listNode1);
+        System.out.println("reverseList = " + reverseList);
+    }
+}
