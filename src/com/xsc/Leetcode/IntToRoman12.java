269b2daff10333dbf9ea4ba6e95cf2ef63e506da
jakesc <649199985@qq.com>
2022-05-02 18:42:02 +0800

    LeetCode12 LeetCode13

---
+++ src/com/xsc/Leetcode/IntToRoman12.java
@@ -0,0 +1,24 @@
+package com.xsc.Leetcode;
+
+/**
+ * 贪心算法
+ *
+ * @author Jakexsc
+ * 2022/5/2
+ */
+public class IntToRoman12 {
+    int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    String[] romanStr = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+    public String intToRoman(int num) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < values.length; i++) {
+            int value = values[i];
+            while (num >= value) {
+                sb.append(romanStr[i]);
+                num -= value;
+            }
+        }
+        return sb.toString();
+    }
+}
---
+++ src/com/xsc/Leetcode/RomanToInt13.java
@@ -0,0 +1,40 @@
+package com.xsc.Leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Jakexsc
+ * 2022/5/2
+ */
+public class RomanToInt13 {
+    Map<Character, Integer> valueMap = new HashMap<Character, Integer>() {{
+        put('I', 1);
+        put('V', 5);
+        put('X', 10);
+        put('L', 50);
+        put('C', 100);
+        put('D', 500);
+        put('M', 1000);
+    }};
+
+    public int romanToInt(String s) {
+        int result = 0;
+        int length = s.length();
+        for (int i = 0; i < length; i++) {
+            Integer value = valueMap.get(s.charAt(i));
+            if (i < length - 1 && value < valueMap.get(s.charAt(i + 1))) {
+                result -= value;
+            } else {
+                result += value;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        RomanToInt13 romanToInt13 = new RomanToInt13();
+        int i = romanToInt13.romanToInt("MCMXCIV");
+        System.out.println(i);
+    }
+}
